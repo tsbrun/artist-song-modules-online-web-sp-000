c4c5a45b83db02a86edabcfaf701327abf843435
Anya Brun <tsbrun@icloud.com>
2020-08-18 18:28:49 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,5 +1,13 @@
 module Findable
 
+  def self.find_by_name(name)
+    @@songs.detect{|a| a.name == name}
+  end
+
+  def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
+  end
+
   def find_by_name(name)
     self.all.select {|element| element.name}
   end
