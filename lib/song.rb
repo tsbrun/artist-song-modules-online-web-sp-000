ad83ebc5fa9c2b9821fefc36413e76e978ee34c2
Anya Brun <tsbrun@icloud.com>
2020-08-18 18:32:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Song
   extend Memorable
-  extend Findable 
+  extend Findable
 
   attr_accessor :name
   attr_reader :artist
@@ -21,7 +21,4 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
